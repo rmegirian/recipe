e1464084f7654edc89e6b868919e768fc8f9ca13
rmegirian <rose.megirian@gmail.com>
2025-04-03 11:48:35 +0800

    Use supplied filename

--- goostats.sh
+++ goostats.sh
@@ -1,6 +1,7 @@
 # load a given file
-# compute the min/max/range of values in a file
-# Load a given file
+fname=$1
+echo "Working with ${fname}"
+echo "Welcome to Nelle's stats script"
 # Compute the min/max/range of values in a file
 
 
